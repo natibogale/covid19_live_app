a124fad498901a40ce6c53627a2371ab41ec9e5f
nati <natibogale45@gmail.com>
2020-12-27 09:38:35 +0300

    edited secret key

--- django_covid19_live_app/settings.py
+++ django_covid19_live_app/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_KEY')
+SECRET_KEY = 'r#m=uvl=)v^8x=e$bmvladkezjrr_)z3qlz5$1*z*rv#vk$s*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
